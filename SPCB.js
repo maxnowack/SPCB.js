b4524e4156652eaecc3697bb405740707e5906c5
Max Nowack <max@unsou.de>
2014-03-12 15:20:41 +0100

    Update SPCB.js

--- SPCB.js
+++ SPCB.js
@@ -49,8 +49,46 @@
 
 
 ExecuteOrDelayUntilScriptLoaded(function(){
-		var ctx = SPCB();
+		/*var ctx = SPCB();
 		ctx.getLists(function(err,lists){
 			var list = lists[0];
+		});*/
+		
+		SP.ClientContext.prototype.loadObjects = function(obj,callback){
+			if(obj instanceof Array) {
+				for(var i=0;i<obj.length;i++)
+				{
+					this.load(obj[i]);
+				}
+			} else {
+				this.load(obj);
+			}
+			
+			this.executeQueryAsync(Function.createDelegate(this,function(sender,args){
+				callback(args,obj);
+			}),Function.createDelegate(this,function(sender,args){
+				callback(args,obj);
+			}));
+		}
+		
+		var ctx = SP.ClientContext.get_current();
+		var web = ctx.get_web();
+		
+		/*ctx.loadObjects(web.get_lists(),function(err,lists){
+			console.log("first call");
+			console.log(err);
+			var e = lists.getEnumerator();
+			
+			while(e.moveNext()){
+				console.log(e.get_current().get_title());
+			}
+		});*/
+		
+		ctx.loadObjects([ctx.get_web(),web.get_lists()],function(err,arr){
+			console.log("second call");
+			console.log(err);
+			console.log(arr[0].isPropertyAvailable('description'));
+			console.log(arr[1][0].get_title());
+			
 		});
 },"sp.js")
